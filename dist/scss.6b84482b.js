e2476a5d80fde1583a0b0fe9f9062a5fc31b9068
h0r4ci0 <horamalean@protonmail.com>
2024-01-04 13:51:58 +0100

    beta

--- dist/scss.6b84482b.js
+++ dist/scss.6b84482b.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46239" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43733" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
